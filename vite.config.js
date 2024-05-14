942dd30e6aa0684a0d19e32b2014bc77cd1a7fcb
Hardelle Briones <hardelle.briones@cit.edu>
2024-05-14 21:37:11 +0800

    comment out proxy

--- vite.config.js
+++ vite.config.js
@@ -6,13 +6,13 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
-    proxy: {
-      "/api": {
-        target:
-          "https://my-json-server.typicode.com/HardelleBriones/backend-json-server",
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ""),
-      },
-    },
+    // proxy: {
+    //   "/api": {
+    //     target:
+    //       "https://my-json-server.typicode.com/HardelleBriones/backend-json-server",
+    //     changeOrigin: true,
+    //     rewrite: (path) => path.replace(/^\/api/, ""),
+    //   },
+    // },
   },
 });
