b8794a8df27b252c29647f74a57c37d533630596
Hardelle Briones <hardelle.briones@cit.edu>
2024-05-14 18:45:55 +0800

    Show more functionality and fixed resizing problem

--- src/component/TaskList.jsx
+++ src/component/TaskList.jsx
@@ -5,7 +5,7 @@ import { toast } from "react-toastify";
 import { useContext } from "react";
 import { ThemeContext } from "../App";
 import { fetchTasks, deleteTask } from "../api/api";
-
+import relax from "../assets/relax.jpg";
 const TaskList = () => {
   const handleOnClickDone = (id) => {
     try {
@@ -37,38 +37,52 @@ const TaskList = () => {
 
   const darkMode = () => {
     return darkTheme
-      ? "flex flex-col items-center bg-gray-500 max-h-full"
-      : "flex flex-col items-center bg-white max-h-full";
+      ? "flex flex-col items-center bg-gray-500 min-h-screen w-full"
+      : "flex flex-col items-center bg-white min-h-screen w-full";
   };
-
+  const [showFullNote, setShowFullNote] = useState(false);
+  const maxLength = 100; // Define the character limit for truncation
+  const toggleShowFullNote = () => {
+    setShowFullNote(!showFullNote);
+  };
   return (
     <>
       <div className={darkMode()}>
-        <div className="w-3/4">
-          <h1 className="my-6 font-medium text-5xl">Todo Items</h1>
-          <div>
-            {todo.map((task) => (
-              <div
-                className="flex flex-row bg-gray-200 rounded-lg mb-4 hover:bg-blue-400"
-                key={task.id}
-              >
-                <div className="w-60 px-6 py-4">{task.task}</div>
-                <div className="w-2/3 py-4 px-6">
-                  <p className="text-gray-800">{task.description}</p>
-                </div>
-                <div className="w-1/6 px-6 py-4 text-center">
-                  <p className="text-gray-800">{task.date}</p>
-                </div>
-                <div className="w-1/6 px-6 py-4 flex justify-center">
-                  <FaCheckCircle
-                    size={30}
-                    className="text-green-400 transition duration-200 ease-in-out hover:text-green-600 cursor-pointer"
-                    onClick={() => handleOnClickDone(task.id)}
-                  />
-                </div>
+        <div className="w-3/4 max-w-full ">
+          <h1 className="my-6 font-medium text-5xl">Todo Items:</h1>
+          {todo.map((task) => (
+            <div
+              className="flex flex-row bg-gray-200 rounded-lg mb-4 hover:bg-blue-400"
+              key={task.id}
+            >
+              <div className="w-1/4 px-6 py-4">{task.task}</div>
+              <div className="w-1/2 px-6 py-4 break-words">
+                <p className="text-gray-800">
+                  {showFullNote || task.note.length <= maxLength
+                    ? task.note
+                    : `${task.note.substring(0, maxLength)}...`}
+                </p>
+                {task.note.length > maxLength && (
+                  <button
+                    className="text-indigo-600 hover:text-indigo-800 mt-2 focus:outline-none focus:underline"
+                    onClick={toggleShowFullNote}
+                  >
+                    {showFullNote ? "Show less" : "Show more"}
+                  </button>
+                )}
               </div>
-            ))}
-          </div>
+              <div className="w-1/6 px-6 py-4 text-center">
+                <p className="text-gray-800">{task.date}</p>
+              </div>
+              <div className="w-1/12 px-6 py-4 flex justify-center">
+                <FaCheckCircle
+                  size={30}
+                  className="text-green-400 transition duration-200 ease-in-out hover:text-green-600 cursor-pointer"
+                  onClick={() => handleOnClickDone(task.id)}
+                />
+              </div>
+            </div>
+          ))}
         </div>
       </div>
     </>
